db134c32b3b9618baeda92515735b04df3efbacb
Md Mridul Hossain <59077783+mrid2110@users.noreply.github.com>
2020-11-19 12:18:57 +0600

    First Commit

---
+++ Footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<div style="width: 100%; border-top: 2px solid gray; position: absolute; bottom: 30px;"></div>
+	<div style="position: absolute; bottom: 5px; padding-left: 45%"><?php echo "Copyright 2017" ?></div>
+
+</body>
+</html>
