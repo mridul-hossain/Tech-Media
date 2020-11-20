ef6bce4496f208f2d5b32b161f1a07b15f118dfc
NafisianCastle <nafis.cseaiub@gmail.com>
2020-11-20 14:55:41 +0600

    change

--- admin/adminDashboard.php
+++ admin/adminDashboard.php
@@ -15,7 +15,6 @@ session_start();
 <head>
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link rel="stylesheet" type="text/css" href="../basicstyling.css">
      <link rel="stylesheet" type="text/css" href="adminDashboard.css">
 </head>
 
