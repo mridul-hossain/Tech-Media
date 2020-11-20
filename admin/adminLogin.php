f1385565b602cc4f2cde1cb80baed5fc802e9875
NafisianCastle <nafis.cseaiub@gmail.com>
2020-11-20 17:48:39 +0600

    modified

--- admin/adminLogin.php
+++ admin/adminLogin.php
@@ -11,30 +11,29 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="../basicstyling.css">
     <link rel="stylesheet" type="text/css" href="adminLogin.css">
 </head>
 
 <body>
     <div class="container form-input" id="login_form_admin">
         <form method="post" action="../controllers/adminLoginValidation.php">
-            <h2 class="center_align upper_txt">Adminstrator area</h2>
+            <h2 class="center_align upper_txt">Adminstrator Login</h2>
 
             <div class="credential_input">
-
-                <label class="med_font">username</label>
+                <br><br>
+                <label class="med_font">Username</label>
                 <br>
                 <input type="text" name="username" placeholder="Enter Admin Username">
                 <br>
                 <label class="sml_font text_error"> <?php echo $usernameErr; ?></label>
                 <br>
-                <label class="med_font">password</label>
+                <label class="med_font">Password</label>
                 <br>
                 <input type="password" name="password" placeholder="Enter Admin Password">
                 <br>
                 <label class="sml_font text_error"> <?php echo $passwordErr; ?></label>
-                <div class="btn btn_danger login_btn">
-                    <input type="submit" name="login" value="login">
+                <div class="login_btn">
+                    <input type="submit" name="login" value="Login">
                 </div>
 
             </div>
