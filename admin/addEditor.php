97558e9d19d5ab4ea05ceab1acf3ffd634198f18
NafisianCastle <nafis.cseaiub@gmail.com>
2020-11-21 16:29:57 +0600

    dummy data added

--- admin/addEditor.php
+++ admin/addEditor.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -9,17 +13,41 @@
 <body>
     <?php
     include 'header_admin.html';
+    include 'sidebar_admin.html';
     ?>
-    <div class="sidenav">
-        <a href="adminDashboard.php">Home</a>
-        <a href="editorsData.php">Editors Data</a>
-        <a href="usersData.php">Users Data</a>
-        <a href="addAdmin.php">Add Admin</a>
-        <a href="addEditor.php">Add Editor</a>
-    </div>
 
     <div class="main">
+        <form action="controller/createUser.php" method="POST" enctype="multipart/form-data">
+            <fieldset>
+                <legend>Add Editor  </legend>
+                <label for="name">Name:</label><br>
+                <input type="text" id="name" name="name"><br>
+                <label for="email">Email:</label><br>
+                <input type="email" id="email" name="email"><br>
+                <label for="phone">Phone:</label><br>
+                <input type="text" id="phone" name="phone"><br>
+                <label for="address">Address:</label><br>
+                <input type="text" id="address" name="address"><br>
+                <label for="city">City:</label><br>
+                <select name="city" id="city">
+                    <option value="empty">Select your city:</option>
+                    <option value="dhaka">Dhaka</option>
+                </select><br>
+                <label for="country">Select your country:</label><br>
+                <select name="country" id="country">
+                    <option value="empty">Select your country:</option>
+                    <option value="bd">Bangladesh</option>
+                </select><br>
+                <label for="birthday">Birthday:</label><br>
+                <input type="date" id="birthday" name="birthday"><br>
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password"><br><br>
+                <hr>
+                <input type="submit" name="createUser" value="Create">
+                <input type="reset">
+            </fieldset>
 
+        </form>
     </div>
 
 </body>
--- admin/editorsData.php
+++ admin/editorsData.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -9,17 +13,100 @@
 <body>
     <?php
     include 'header_admin.html';
+    include 'sidebar_admin.html';
     ?>
-    <div class="sidenav">
-        <a href="adminDashboard.php">Home</a>
-        <a href="editorsData.php">Editors Data</a>
-        <a href="usersData.php">Users Data</a>
-        <a href="addAdmin.php">Add Admin</a>
-        <a href="addEditor.php">Add Editor</a>
-    </div>
 
     <div class="main">
+        <form>
+            <table style="border: 2px solid black;">
+                <thead>
+                    <tr>
+                        <th style="border: 2px solid black;">ID</th>
+                        <th style="border: 2px solid black;">Username</th>
+                        <th style="border: 2px solid black;">Name</th>
+                        <th style="border: 2px solid black;">Password</th>
+                        <th style="border: 2px solid black;">Email</th>
+                        <th style="border: 2px solid black;">Phone</th>
+                        <th style="border: 2px solid black;">Address</th>
+                        <th style="border: 2px solid black;">City</th>
+                        <th style="border: 2px solid black;">Counry</th>
 
+                        <th style="border: 2px solid black;">DOB</th>
+                        <th style="border: 2px solid black;">Image</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th style="border: 2px solid black;">1</th>
+                        <th style="border: 2px solid black;">nafis</th>
+                        <th style="border: 2px solid black;">nafisur Rahman</th>
+                        <th style="border: 2px solid black;">1234</th>
+                        <th style="border: 2px solid black;">nfa@gmail.com</th>
+                        <th style="border: 2px solid black;">012345678902</th>
+                        <th style="border: 2px solid black;">khilgaon</th>
+                        <th style="border: 2px solid black;">Dhaka</th>
+                        <th style="border: 2px solid black;">Bangladesh</th>
+                        <th style="border: 2px solid black;">12-03-99</th>
+                        <th style="border: 2px solid black;">null</th>
+
+                        <td style="border: 2px solid black;"><a href="editProduct.php?name=<?php echo $product['Name'] ?>">Edit</a></td>
+                        <td style="border: 2px solid black;"><a href="deleteProduct.php?name=<?php echo $product['Name'] ?>">Delete</a></td>
+
+                    </tr>
+                    <tr>
+                        <th style="border: 2px solid black;">2</th>
+                        <th style="border: 2px solid black;">nafis</th>
+                        <th style="border: 2px solid black;">nafisur Rahman</th>
+                        <th style="border: 2px solid black;">1234</th>
+                        <th style="border: 2px solid black;">nfa@gmail.com</th>
+                        <th style="border: 2px solid black;">012345678902</th>
+                        <th style="border: 2px solid black;">khilgaon</th>
+                        <th style="border: 2px solid black;">Dhaka</th>
+                        <th style="border: 2px solid black;">Bangladesh</th>
+                        <th style="border: 2px solid black;">12-03-99</th>
+                        <th style="border: 2px solid black;">null</th>
+
+                        <td style="border: 2px solid black;"><a href="editProduct.php?name=<?php echo $product['Name'] ?>">Edit</a></td>
+                        <td style="border: 2px solid black;"><a href="deleteProduct.php?name=<?php echo $product['Name'] ?>">Delete</a></td>
+
+                    </tr>
+                    <tr>
+                        <th style="border: 2px solid black;">3</th>
+                        <th style="border: 2px solid black;">nafis</th>
+                        <th style="border: 2px solid black;">nafisur Rahman</th>
+                        <th style="border: 2px solid black;">1234</th>
+                        <th style="border: 2px solid black;">nfa@gmail.com</th>
+                        <th style="border: 2px solid black;">012345678902</th>
+                        <th style="border: 2px solid black;">khilgaon</th>
+                        <th style="border: 2px solid black;">Dhaka</th>
+                        <th style="border: 2px solid black;">Bangladesh</th>
+                        <th style="border: 2px solid black;">12-03-99</th>
+                        <th style="border: 2px solid black;">null</th>
+
+                        <td style="border: 2px solid black;"><a href="editProduct.php?name=<?php echo $product['Name'] ?>">Edit</a></td>
+                        <td style="border: 2px solid black;"><a href="deleteProduct.php?name=<?php echo $product['Name'] ?>">Delete</a></td>
+
+                    </tr>
+                    <tr>
+                        <th style="border: 2px solid black;">4</th>
+                        <th style="border: 2px solid black;">nafis</th>
+                        <th style="border: 2px solid black;">nafisur Rahman</th>
+                        <th style="border: 2px solid black;">1234</th>
+                        <th style="border: 2px solid black;">nfa@gmail.com</th>
+                        <th style="border: 2px solid black;">012345678902</th>
+                        <th style="border: 2px solid black;">khilgaon</th>
+                        <th style="border: 2px solid black;">Dhaka</th>
+                        <th style="border: 2px solid black;">Bangladesh</th>
+                        <th style="border: 2px solid black;">12-03-99</th>
+                        <th style="border: 2px solid black;">null</th>
+
+                        <td style="border: 2px solid black;"><a href="editProduct.php?name=<?php echo $product['Name'] ?>">Edit</a></td>
+                        <td style="border: 2px solid black;"><a href="deleteProduct.php?name=<?php echo $product['Name'] ?>">Delete</a></td>
+
+                    </tr>
+                </tbody>
+            </table>
+        </form>
     </div>
 
 </body>
