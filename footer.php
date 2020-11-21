4f07110eaf396090f83126b27f9190f4f4179fe9
NafisianCastle <nafis.cseaiub@gmail.com>
2020-11-21 19:54:14 +0600

    footer setting

--- footer.php
+++ footer.php
@@ -5,13 +5,14 @@
 	<title></title>
 	<style>
 		.footer {
-			position: fixed;
 			left: 0;
 			bottom: 0;
 			width: 100%;
 			background-color: #f1f1f1;
 			color: dodgerblue;
 			text-align: center;
+			height: 50px;
+			display: inline-block;
 		}
 	</style>
 </head>
