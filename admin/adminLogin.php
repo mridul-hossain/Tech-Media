ccc246d06459b2dce836f5eb480faa36a2b2d54b
NafisianCastle <nafis.cseaiub@gmail.com>
2020-11-30 13:51:34 +0600

    login successfully done

--- admin/adminLogin.php
+++ admin/adminLogin.php
@@ -15,37 +15,27 @@ session_start();
 
     <div class="container form-input" id="login_form_admin">
         <form method="post" action="controllers/adminLoginControl.php">
-            <?php
-            $_SESSION['username'] = 'nafis';
-            $_SESSION['password'] = '1234';
-            ?>
             <h2 class="center_align upper_txt">Adminstrator Login</h2>
-
             <div class="credential_input">
                 <br><br>
                 <label class="med_font">Username</label>
                 <br>
-                <input type="text" name="name" placeholder="Enter Admin Username" required>
+                <input type="text" name="username" placeholder="Enter Admin Username">
                 <br>
                 <label class="sml_font text_error"> <?php echo $usernameErr; ?></label>
                 <br>
                 <label class="med_font">Password</label>
                 <br>
-                <input type="text" name="password" placeholder="Enter Admin Password" required>
+                <input type="text" name="password" placeholder="Enter Admin Password">
                 <br>
                 <label class="sml_font text_error"> <?php echo $passwordErr; ?></label>
-                <input type="hidden" name="uname" value="<?php echo $_SESSION['username'] ?>">
-                <input type="hidden" name="pass" value="<?php echo $_SESSION['password'] ?>">
                 <div class="login_btn">
                     <input type="submit" name="submit" value="Login">
                 </div>
-
             </div>
 
         </form>
     </div>
-
 </body>
-
 
 </html>
--- admin/controllers/adminLoginControl.php
+++ admin/controllers/adminLoginControl.php
@@ -1,47 +1,30 @@
-<?php  
+<?php
 session_start();
-if(isset($_SESSION["admin_name"]))
-{
- header("location:home.php");
-}
-$connect = mysqli_connect("localhost", "root", "2524", "testing");  
-if(isset($_POST["login"]))   
-{  
- if(!empty($_POST["member_name"]) && !empty($_POST["member_password"]))
- {
-  $name = mysqli_real_escape_string($connect, $_POST["member_name"]);
-  $password = md5(mysqli_real_escape_string($connect, $_POST["member_password"]));
-  $sql = "Select * from admin_login where admin_name = '" . $name . "' and admin_password = '" . $password . "'";  
-  $result = mysqli_query($connect,$sql);  
-  $user = mysqli_fetch_array($result);  
-  if($user)   
-  {  
-   if(!empty($_POST["remember"]))   
-   {  
-    setcookie ("member_login",$name,time()+ (10 * 365 * 24 * 60 * 60));  
-    setcookie ("member_password",$password,time()+ (10 * 365 * 24 * 60 * 60));
-    $_SESSION["admin_name"] = $name;
-   }  
-   else  
-   {  
-    if(isset($_COOKIE["member_login"]))   
-    {  
-     setcookie ("member_login","");  
-    }  
-    if(isset($_COOKIE["member_password"]))   
-    {  
-     setcookie ("member_password","");  
-    }  
-   }  
-   header("location:home.php"); 
-  }  
-  else  
-  {  
-   $message = "Invalid Login";  
-  } 
- }
- else
- {
-  $message = "Both are Required Fields";
- }
+include("../../dbConnect.php");
+
+try {
+    $connect = db_conn();
+    if (isset($_POST["submit"])) {
+        if (empty($_POST["username"]) || empty($_POST["password"])) {
+            echo '<label>All fields are required</label>';
+        } else {
+            $query = "SELECT * FROM admin WHERE username = :username AND pass = :password";
+            $statement = $connect->prepare($query);
+            $statement->execute(
+                array(
+                    'username'     =>     $_POST["username"],
+                    'password'     =>     $_POST["password"]
+                )
+            );
+            $count = $statement->rowCount();
+            if ($count > 0) {
+                $_SESSION["username"] = $_POST["username"];
+                header("location:../adminDashboard.php");
+            } else {
+                echo '<label>Wrong Data</label>';
+            }
+        }
+    }
+} catch (PDOException $error) {
+    echo $error->getMessage();
 }
