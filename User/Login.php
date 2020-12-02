033ab07b6e33caddea37e57760a1f2600c1465c5
MariamIslam <63836484+MariamIslam@users.noreply.github.com>
2020-12-02 19:26:32 +0600

    Add files via upload

---
+++ User/Login.php
@@ -0,0 +1,51 @@
+<?php
+
+session_start();
+
+$_SESSION['username'] = 'Mariam';
+		$_SESSION['password'] = '5555';
+?>
+
+
+<!DOCTYPE HTML>
+<html>  
+<body>
+<?php
+	include 'Top.php';
+	?>
+
+	
+<form action="controller/profilrControl.php" method="post">
+
+       
+		
+		
+<div style="position: relative; padding-left: 30%">
+<fieldset style="width:50%" align="center">
+<legend>Login</legend>
+<form>
+
+
+
+Name: <input type="text" name="name"  ><br>
+Password: <input type="text" name="password"><br>
+<hr>
+
+<input type="submit" name="submit">
+<p><a href="User_Registration.php">Forget Password?</a></p>
+<input type="hidden" name="uname" value="<?php echo $_SESSION['username']?>">
+<input type="hidden" name="pass" value="<?php echo $_SESSION['password']?>">
+			
+</form>
+</fieldset>
+</div>
+</form>
+<br>
+<br>
+
+<?php
+	include 'Abid.php';
+	?>
+
+</body>
+</html>
---
+++ User/post.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+<?php
+	include 'Top.php';
+	?>
+
+	
+<form action="/action_page.php">
+<label for="w3review">Post:</label>
+<textarea id="w3review" name="w3review" rows="4" cols="50">
+  
+  </textarea>
+  <br><br>
+  <input type="submit" value="Submit">
+  
+  Attach Video:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  Attach image:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+</form>	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	<?php
+	include 'Abid.php';
+	?>
+
+</body>
+</html>
