79baa4c4519eb37a2c2989d3fc564b3748e9f0a2
MariamIslam <63836484+MariamIslam@users.noreply.github.com>
2020-12-02 19:24:31 +0600

    Add files via upload

---
+++ User/Controller/Editcontrol.php
@@ -0,0 +1,59 @@
+<?php
+
+
+
+require_once '../Profile.php';
+
+if(isset($_POST["save"])) {
+	$target_dir = "../Media/logo.png";
+$target_file = $target_dir . basename($_FILES["photo"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+  $check = getimagesize($_FILES["photo"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+  if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+if ($_FILES["photo"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+
+} else {
+  if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["photo"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
