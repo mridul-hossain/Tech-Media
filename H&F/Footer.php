c5d6cbecbaf2e1119a15ffd5d8d74eda0da0f45b
Md Mridul Hossain <59077783+mrid2110@users.noreply.github.com>
2020-12-13 20:57:23 +0600

    Add files via upload

---
+++ H&F/Footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="Footer.css">
+</head>
+<body>
+
+	<div class="footer">
+		<div class="footerContent">
+			<label>Copyright 2020</label>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ H&F/Header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="Header.css?version=51">
+	<link rel="stylesheet" type="text/css" href="fontawesome-free-5.15.1-web/css/all.css">
+</head>
+<body>
+
+	
+	<div class="box">
+		<div class="logo">
+		<label>Tech Media</label>
+	</div>
+	<div class="searchBox">
+		<i class="fas fa-search"></i>
+		<input type="text" name="search" placeholder="Search">
+	</div>
+	<div>
+		<table>
+			<tr>
+				<td><a href="#" class="content">Home</a></td>
+				<td><a href="#" class="content">Registration</a></td>
+				<td><a href="#" class="content">Login</a></td>
+			</tr>
+		</table>
+	</div>
+	</div>
+
+</body>
+</html>
