fe9393dc09561ecec6db7e89f9024bdba91ba759
NafisianCastle <nafis.cseaiub@gmail.com>
2021-01-02 12:10:25 +0600

    mod

--- admin/controllers/createEditor.php
+++ admin/controllers/createEditor.php
@@ -1,4 +1,50 @@
 <?php
-require_once '../model/model.php';
+require_once '../Model_.php';
+
+if (isset($_POST['Submit'])) {
 
 
+    $data['pass'] = $_POST['password'];
+    $data['name'] = $_POST['name'];
+    $data['phone'] = $_POST['phone'];
+    $data['email'] = $_POST['email'];
+    $data['city'] = $_POST['city'];
+    $data['country'] = $_POST['country'];
+    $data['dob'] = $_POST['dob'];
+    $data['username'] = $_POST['username'];
+    $data['gender'] = $_POST['gender'];
+
+
+    if (empty($_POST["name"]) || !preg_match("/^[a-zA-Z\s]+$/", $_POST["name"]) || str_word_count($_POST["name"]) < 2) {
+        header('Location: ../view/addEditor.php?nameErr=*Give a valid name.');
+    } else if (empty($_POST["email"])) {
+        header('Location: ../view/addEditor.php?emailErr=*Type a valid Email.');
+    } else if (empty($_POST["username"])) {
+        header('Location: ../view/addEditor.php?usernameErr=*Please select give a username!');
+    } else if (empty($_POST["password"])) {
+        header('Location: ../view/addEditor.php?passwordErr=*Please give a password.');
+    } else if (empty($_POST["gender"])) {
+        header('Location: ../view/addEditor.php?genderErr=*Gender is required');
+    } else if (empty($_POST["dob"]) || $_POST["dob"] >= date('Y-m-d')) {
+        header('Location: ../view/addEditor.php?dobErr=*Give your valid birthdate.');
+    } else if (empty($_POST["password"]) || $_POST["password"] != $_POST["cpassword"]) {
+        header('Location: ../view/addEditor.php?cpasswordErr=*Password does not match.');
+    } else if (empty($_POST["city"])) {
+        header('Location: ../view/addEditor.php?cityErr=*Give your city name.');
+    } else if (empty($_POST["country"])) {
+        header('Location: ../view/addEditor.php?countryErr=*Give your country name.');
+    } else if (empty($_POST["phone"])) {
+        header('Location: ../view/addEditor.php?phoneErr=*Type your phone number.');
+    } else {
+        if (addEditor($data)) {
+            header('Location: ../Login.php');
+        }
+    }
+
+    // if(addEditor($data))
+    // {
+    // 	header('Location: ../view/addEditor.php');
+    // }
+} else {
+    echo "Please try again!";
+}
--- admin/view/addEditor.php
+++ admin/view/addEditor.php
@@ -1,72 +1,103 @@
-<?php
-session_start();
-if ($_SESSION['username'] == "" || $_SESSION["usertype"] != "admin") {
-    header("location:adminLogin.php");
-}
-?>
 <!DOCTYPE html>
 <html>
 
 <head>
+    <title></title>
+    <style>
+        .names {
+            width: 100px;
+            text-align: right;
+            margin: auto;
+            float: left;
+        }
+
+        .items {
+            width: 500px;
+            margin-left: 105px;
+            text-align: left;
+            vertical-align: top;
+        }
+
+        .line {
+            width: 400px;
+            border-top: 2px solid black;
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <title></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/profile.css">
+    <link rel="stylesheet" type="text/css" href="css/basicStyling.css">
     <link rel="stylesheet" type="text/css" href="css/adminDashboard.css">
     <link rel="stylesheet" type="text/css" href="css/header_admin.css">
     <link rel="stylesheet" type="text/css" href="css/sidebar_admin.css">
     <link rel="stylesheet" type="text/css" href="../../HnF/Footer.css">
+    <script type="text/javascript" src="js/profile.js"></script>
 </head>
 
 <body>
+
     <?php
-    include 'header_admin.php';
     include 'sidebar_admin.html';
+    include 'header_admin.php';
     ?>
-    <div class="main">
-        <form action="controllers/createEditor.php" method="POST" enctype="multipart/form-data">
-            <fieldset style="border-color: dodgerblue; border-radius: 8px;border-style: inset;">
-                <legend>Add Editor </legend>
-                <table>
-                    <tr>
-                        <td>
-                            <label for="uname">Username:</label><br>
-                            <input type="text" id="uname" name="uname"><br>
-                            <label for="name">Name:</label><br>
-                            <input type="text" id="name" name="name"><br>
-                            <label for="email">Email:</label><br>
-                            <input type="email" id="email" name="email"><br>
-                            <label for="phone">Phone:</label><br>
-                            <input type="text" id="phone" name="phone"><br>
-                            <label for="address">Address:</label><br>
-                            <input type="text" id="address" name="address"><br>
-                            <label for="city">City:</label><br>
-                            <select name="city" id="city">
-                                <option value="empty">Select your city:</option>
-                                <option value="dhaka">Dhaka</option>
-                            </select><br>
-                            <label for="country">Select your country:</label><br>
-                            <select name="country" id="country">
-                                <option value="empty">Select your country:</option>
-                                <option value="bd">Bangladesh</option>
-                            </select><br>
-                            <label for="birthday">Birthday:</label><br>
-                            <input type="date" id="birthday" name="birthday"><br>
-                            <label for="password">Password:</label><br>
-                            <input type="password" id="password" name="password"><br><br>
-                            <input type="submit" name="createEditor" value="Create">
-                            <input type="reset">
-                        </td>
-                        <td>
-                            <label for="fileToUpload">Upload Image:</label>
-                            <input type="file" name="fileToUpload" id="fileToUpload">
-                        </td>
-                    </tr>
-                </table>
+    <form method="post" action="../controllers/createEditor.php">
+        <div style="position: relative; padding-left: 30%; padding-top: 20px;">
+            <fieldset style="width: 30%; box-shadow: 0 0 20px;">
+                <div class="names">
+                    <label>Name: </label><br><br>
+                    <label style="margin-bottom: 2px">Email: </label><br><br>
+                    <label>Gender: </label><br><br><br>
+                    <label>User Name: </label><br><br>
+                    <label>Password: </label><br><br>
+                    <label>Confirm Password: </label><br><br>
+                    <label>Date of Birth: </label><br><br>
+                    <label>City: </label><br><br>
+                    <label>Country: </label><br><br>
+                    <label>Phone: </label><br><br>
+                </div>
+                <div class="items">
+                    <input type="text" name="name">
+                    <span><?php if (isset($_GET['nameErr'])) echo $_GET['nameErr']; ?><br><br>
+                        <input type="email" name="email" style="position: relative; bottom: 3px">
+                        <span><?php if (isset($_GET['emailErr'])) echo $_GET['emailErr']; ?></span><br><br>
+                        <input type="radio" id="male" name="gender" value="Male" style="position: relative; bottom: 6px">
+                        <label for="male" style="position: relative; bottom: 6px">Male</label>
+                        <input type="radio" id="female" name="gender" value="Female" style="position: relative; bottom: 6px">
+                        <label for="female" style="position: relative; bottom: 6px">Female</label>
+                        <span><?php if (isset($_GET['genderErr'])) echo $_GET['genderErr']; ?><br><br>
+                            <input type="text" name="username" style="position: relative; bottom: 8px">
+                            <span><?php if (isset($_GET['usernameErr'])) echo $_GET['usernameErr']; ?><br><br>
+                                <input type="text" name="password" style="position: relative; bottom: 8px">
+                                <span><?php if (isset($_GET['passwordErr'])) echo $_GET['passwordErr']; ?><br><br>
+                                    <input type="text" name="cpassword" style="position: relative; bottom: 8px">
+                                    <span><?php if (isset($_GET['cpasswordErr'])) echo $_GET['cpasswordErr']; ?><br><br>
+
+
+
+
+
+                                        <div style="position: relative; bottom: 2px">
+                                            <input type="date" name="dob" style="float: left;">
+                                        </div>
+                                        <span><?php if (isset($_GET['dobErr'])) echo $_GET['dobErr']; ?>
+
+                </div><br><br>
+                <input type="text" name="city" style="position: relative; bottom: 1px">
+                <span><?php if (isset($_GET['cityErr'])) echo $_GET['cityErr']; ?><br><br>
+                    <input type="text" name="country" style="position: relative; bottom: 1px">
+                    <span><?php if (isset($_GET['countryErr'])) echo $_GET['countryErr']; ?><br><br>
+                        <input type="text" name="phone" style="position: relative; bottom: 1px">
+                        <span><?php if (isset($_GET['phoneErr'])) echo $_GET['phoneErr']; ?><br><br>
+
+                            <input type="submit" name="Submit" style="position: relative; float: left;">
             </fieldset>
-            <br>
-        </form>
-    </div>
-    <?php
-    include '../../HnF/Footer.php';
-    ?>
+        </div>
+
+
+
+    </form>
+
 </body>
 
 </html>
