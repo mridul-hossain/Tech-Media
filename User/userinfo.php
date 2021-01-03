5b34880c11683a1f66d610c25678c1780f304a58
MariamIslam <63836484+MariamIslam@users.noreply.github.com>
2021-01-03 18:46:03 +0600

    Delete userinfo.php

--- User/userinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('./dbconn.php');
-
-$selectquery = "select * from user_info ";
-$result = $conn->query($selectquery);
-foreach ($result as $row) {
-	echo "ID:" . $row['Name'] . "<br><br>";
-}
